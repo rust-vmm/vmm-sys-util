d2680940f9777774ba3ddb0e317ff089c9f9eeb0
Samuel Ortiz <sameo@linux.intel.com>
2019-05-07 00:15:55 +0200

    vmm-sys-util: Add a basic fallocate implementation
    This will be needed for the write_zeroes and seek_hole implementations.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- src/lib.rs
+++ src/lib.rs
@@ -22,4 +22,50 @@ pub use errno::*;
 pub use eventfd::*;
 pub use poll::*;
 
+use std::os::unix::io::AsRawFd;
+
 pub use crate::file_traits::{FileSetLen, FileSync};
+
+pub enum FallocateMode {
+    PunchHole,
+    ZeroRange,
+}
+
+/// Safe wrapper for `fallocate()`.
+pub fn fallocate(
+    file: &dyn AsRawFd,
+    mode: FallocateMode,
+    keep_size: bool,
+    offset: u64,
+    len: u64,
+) -> Result<()> {
+    let offset = if offset > libc::off64_t::max_value() as u64 {
+        return Err(Error::new(libc::EINVAL));
+    } else {
+        offset as libc::off64_t
+    };
+
+    let len = if len > libc::off64_t::max_value() as u64 {
+        return Err(Error::new(libc::EINVAL));
+    } else {
+        len as libc::off64_t
+    };
+
+    let mut mode = match mode {
+        FallocateMode::PunchHole => libc::FALLOC_FL_PUNCH_HOLE,
+        FallocateMode::ZeroRange => libc::FALLOC_FL_ZERO_RANGE,
+    };
+
+    if keep_size {
+        mode |= libc::FALLOC_FL_KEEP_SIZE;
+    }
+
+    // Safe since we pass in a valid fd and fallocate mode, validate offset and len,
+    // and check the return value.
+    let ret = unsafe { libc::fallocate64(file.as_raw_fd(), mode, offset, len) };
+    if ret < 0 {
+        errno_result()
+    } else {
+        Ok(())
+    }
+}
