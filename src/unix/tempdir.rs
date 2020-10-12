cae232038682b2446be51881634943fe564ede9c
u5surf <u5.horie@gmail.com>
2020-10-13 00:16:04 +0900

    tempdir : improve unit tests removing tempdir
    test_remove_dir() is now checking that removing a directory that
    contains other directories are removing those too.
    
    Fixes #101.
    
    Signed-off-by: u5surf <u5.horie@gmail.com>

--- src/unix/tempdir.rs
+++ src/unix/tempdir.rs
@@ -160,12 +160,25 @@ mod tests {
 
     #[test]
     fn test_remove_dir() {
+        use crate::tempfile::TempFile;
         let t = TempDir::new().unwrap();
         let path = t.as_path().to_owned();
         assert!(t.remove().is_ok());
         // Calling remove twice returns error.
         assert!(t.remove().is_err());
         assert!(!path.exists());
+
+        let t = TempDir::new().unwrap();
+        let mut file = TempFile::new_in(t.as_path()).unwrap();
+        let t2 = TempDir::new_in(t.as_path()).unwrap();
+        let mut file2 = TempFile::new_in(t2.as_path()).unwrap();
+        let path2 = t2.as_path().to_owned();
+        assert!(t.remove().is_ok());
+        // Calling t2.remove returns error because parent dir has removed
+        assert!(t2.remove().is_err());
+        assert!(!path2.exists());
+        assert!(file.remove().is_err());
+        assert!(file2.remove().is_err());
     }
 
     #[test]
