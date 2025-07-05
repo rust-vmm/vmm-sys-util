3159b7109d48e2f8f1f93ecadd8f9656985a95a0
Wenyu Huang <huangwenyuu@outlook.com>
2025-07-05 17:38:51 +0800

    Add generic event notification(EventNotifier/Consumer)
    Introduce an abstract event notification in order to support multiple
    platforms. EventNotifier is used to send a notification, and EventConsumer
    is used to receive a notification. The fd used in EventNotifier or
    EventConsumer can be from eventfd, pipefd or others.
    
    Signed-off-by: Wenyu Huang <huangwenyuu@outlook.com>

--- src/unix/mod.rs
+++ src/unix/mod.rs
@@ -1,5 +1,6 @@
 // Copyright 2022 rust-vmm Authors or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: BSD-3-Clause
+pub mod event;
 pub mod file_traits;
 pub mod sock_ctrl_msg;
 pub mod tempdir;
